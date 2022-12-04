680a41fa251d97583b81facb84f2d26f92517329
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-05 00:37:48 +0300

    v7

--- specialcontact/src/App.js
+++ specialcontact/src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PrivateRouter from "./router/PrivateRouter";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      app
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<PrivateRouter />}>
+          <Route path="" element={<Home />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- specialcontact/src/authentication/firebase.js
+++ specialcontact/src/authentication/firebase.js
@@ -11,5 +11,6 @@ const firebaseConfig = {
   };
   
   const app = initializeApp(firebaseConfig);
-  const app = initializeApp(firebaseConfig);
+
+  export const auth = getAuth(app)
   export default app;
