d6120f2d09ec5d662bb1f236b9b5815747195bdb
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-05 00:02:01 +0300

    v6

---
+++ specialcontact/src/authentication/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    databaseURL: process.env.REACT_APP_DATABASEURL
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  export default app;
