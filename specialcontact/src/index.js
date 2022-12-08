7b6c9c962b866f59f4434b0ed04e6913c7742b5c
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-08 15:03:18 +0300

    v23

--- specialcontact/src/index.js
+++ specialcontact/src/index.js
@@ -6,13 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/app/store";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { persistor } from "./redux/app/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-     <Provider store={store}>
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- specialcontact/src/pages/Home.jsx
+++ specialcontact/src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Form from "../components/Form"
-import Table from "../components/Table"
+
+import React from "react";
+import Form from "../components/Form";
+import Table from "../components/Table";
 
 const Home = () => {
   return (
-    <div>
-      <Form/>
-      <Table/>
+    <div
+      style={{
+        backgroundImage: `url("https://picsum.photos/1600/900")`,
+        
+      }}
+    >
+      <Form />
+      <Table />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
