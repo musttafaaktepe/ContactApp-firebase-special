fa06a8623d0801ed6dffb815b5b162d1a6620da7
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-08 15:35:10 +0300

    v24

---
+++ specialcontact/src/notifies/ToastifyNotifies.js
@@ -0,0 +1,51 @@
+
+import {toast} from "react-toastify"
+
+
+export const NotiInfo = (msg) => {
+toast.info((msg), {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });}
+
+export const NotiSucces = (msg) => {
+toast.success((msg), {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });}
+
+export const NotiWarn = (msg) => {
+toast.warn((msg), {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });}
+
+export const NotiErr = (msg) => {
+toast.error((msg), {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });}
